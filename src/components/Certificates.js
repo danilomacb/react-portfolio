09d6c0d369524cc220b3e7ba8e5e24fb534ead7d
danilomacb <danilo.bakun@gmail.com>
2020-05-18 11:32:04 -0300

    creating styles for certificates

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 function Certificates() {
-  return <h1>Certificates</h1>;
+  return (
+    <Container className="my-5">
+      <Row>
+        <Col sm="12" md="6" lg="4">
+          <Card bg="dark" text="light">
+            <Card.Img src={process.env.PUBLIC_URL + "/images/certificates/Html5ECss3-B7web.jpg"} />
+            <Card.Body>
+              <Card.Title className="text-center">HTML5 e CSS3 - B7Web</Card.Title>
+              <Card.Text>
+                <p className="my-card-description">Data de Emissão:</p> 26/02/2019 <br />
+                <p className="my-card-description">Código de Validação:</p> 00009717 <br />
+                <p className="my-card-description">Validar:</p> https://b7web.com.br/certificado/
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Certificates;
